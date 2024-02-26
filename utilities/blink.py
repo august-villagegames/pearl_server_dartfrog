2d441181150b15ed143cbb3a006084df3261001f
August Comstock <augustcomstock@Augusts-MBP.lan>
2024-02-25 21:27:56 -0800

    second sleep

--- utilities/blink.py
+++ utilities/blink.py
@@ -2,17 +2,16 @@
 import RPi.GPIO as GPIO
 import time
 
-message = "gpio py file reached"
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-print(message)
 
 while (True):
     GPIO.output(18, 1)
     time.sleep(1)
     GPIO.output(18, 0)
+    time.sleep(1)
 
 
 GPIO.cleanup()
