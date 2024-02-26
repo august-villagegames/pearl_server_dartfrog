a02c600423e7605198e1326ce41ba32071b31d3e
August Comstock <augustcomstock@Augusts-MBP.lan>
2024-02-25 21:05:41 -0800

    print 2

--- utilities/blink.py
+++ utilities/blink.py
@@ -2,9 +2,11 @@
 import RPi.GPIO as GPIO
 import time
 
+message = "gpio py file reached"
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-print('GPIO py file reached')
+print(message)
 GPIO.output(18, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(18, GPIO.LOW)
