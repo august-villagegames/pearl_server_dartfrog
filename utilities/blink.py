0d33dd0f17b4e6385f6d7b76b5928fc1e30d8f6e
August Comstock <augustcomstock@Augusts-MBP.lan>
2024-03-20 21:12:23 -0700

    removed while loop in blink

--- utilities/blink.py
+++ utilities/blink.py
@@ -7,11 +7,10 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 
-while (True):
-    GPIO.output(18, 1)
-    time.sleep(1)
-    GPIO.output(18, 0)
-    time.sleep(1)
+GPIO.output(18, 1)
+time.sleep(1)
+GPIO.output(18, 0)
+time.sleep(1)
     
 
 GPIO.cleanup()
